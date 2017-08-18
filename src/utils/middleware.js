a027421fbe60c7a0786fb6408935871761a37c8d
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-08-18 11:12:39 +0800

    feat(middleware): add rateLimit feature.

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,6 +1,7 @@
 const R = require('ramda');
 const compress = require('micro-compress');
 const cors = require('micro-cors');
+const rateLimit = require('micro-ratelimit');
 
 const ORIGIN = process.env.ORIGIN;
 
@@ -11,6 +12,7 @@ const middleware = R.compose(
     origin: ORIGIN || '*',
   }),
   compress,
+  R.curry(rateLimit)({ window: 1000, limit: 1 }),
 );
 
 module.exports = middleware;
